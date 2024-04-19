d9f2822e690ced7c395e81e335078042c132ea8e
Jonathan Williams <jcwilliams37@crimson.ua.edu>
2024-04-19 09:22:29 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# maxClique
- 
+# maxClique Project
+## Details
+Created: Friday, April 19th 2024 at 10:12:08
+Author: Jonathan Williams 
+
+## Purpose
+To discover a good max clique solution to a given adjaceny list forming a matrix
---
+++ maxClique.py
@@ -0,0 +1,17 @@
+###############################################################################
+# File: maxClique.py                                                           #
+# Created: Friday, April 19th 2024 at 10:12:08                                 #
+# Author: Jonathan Williams                                                    #
+# -----                                                                        #
+# Last Modified: Friday, April 19th 2024 09:22:16                              #
+# Modified By: Jonathan Williams                                               #
+###############################################################################
+
+
+def main():
+    # Main Code Will Go Here
+    return 0
+
+
+if __name__ == "__main__":
+    main()
