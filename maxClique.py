ef22b4bb7fe913e9d01971081c223ba66b9733cf
Jonathan Williams <jcwilliams37@crimson.ua.edu>
2024-04-21 19:55:00 -0500

    Function to read file, currently hardcoded for faster  coding

--- maxClique.py
+++ maxClique.py
@@ -3,13 +3,46 @@
 # Created: Friday, April 19th 2024 at 10:12:08                                 #
 # Author: Jonathan Williams                                                    #
 # -----                                                                        #
-# Last Modified: Friday, April 19th 2024 09:22:16                              #
+# Last Modified: Sunday, April 21st 2024 19:54:19                              #
 # Modified By: Jonathan Williams                                               #
 ###############################################################################
 
+import bruteForce as bf
+import networkx as nx
+
+import matplotlib.pyplot as plt
+
+import tkinter as tk
+from tkinter import filedialog
+
+
+def getFile():
+    print("Use the file dialog to open a file to use:")
+
+    # Open a file dialog for selecting a file
+    root = tk.Tk()
+    root.withdraw()
+    filePath = filedialog.askopenfilename()
+
+    # Check if a file was selected
+    if filePath:
+        print("Selected file: ", filePath)
+        return filePath
+        # You can do further processing with the selected file here
+    else:
+        return getFile()
+
 
 def main():
-    # Main Code Will Go Here
+
+    filePath = "G/D14G100.adjlist"  # getFile() change back for final
+
+    graph = nx.Graph()
+    graph = nx.read_adjlist(filePath)
+    bf.bruteforce(graph)
+    # Draw the graph
+    # nx.draw(graph, with_labels=True)
+    # plt.show()
     return 0
 
 
