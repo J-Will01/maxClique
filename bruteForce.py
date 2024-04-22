01e50a101d2fb10f521e97277f529a849b72fa00
Jonathan Williams <jcwilliams37@crimson.ua.edu>
2024-04-21 19:54:34 -0500

    Create brute force file

---
+++ bruteForce.py
@@ -0,0 +1,16 @@
+###############################################################################
+# File: bruteForce.py                                                          #
+# Created: Sunday, April 21st 2024 at 17:56:18                                 #
+# Author: Jonathan Williams                                                    #
+# -----                                                                        #
+# Last Modified: Sunday, April 21st 2024 19:53:28                              #
+# Modified By: Jonathan Williams                                               #
+###############################################################################
+
+## Supplemental file to run brute force maximum clique finding in the main file
+import tkinter as tk
+import networkx as nx
+
+
+def bruteforce(graph: nx.Graph):
+    print(graph)
